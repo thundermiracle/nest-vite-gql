55b183a2d81c63352226cce6fc1bc4101d49ae22
thundermiracle <cissoliu@gmail.com>
2022-01-05 22:52:28 +0900

    chore: use Apollo Sandbox instead of playground

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { TweetsModule } from './tweets/tweets.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
-      playground: true,
+      // @see https://docs.nestjs.com/graphql/quick-start#apollo-sandbox
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
       typePaths: ['./**/*.graphql'],
     }),
     TweetsModule,
